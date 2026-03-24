13aebda5a04f577885df9a1613375e146bc08ea8
Ziol3k <jan_ziolo@wp.pl>
2026-03-24 23:04:22 +0100

    3.5 completed

--- zadanie_1/src/main.pas
+++ zadanie_1/src/main.pas
@@ -18,6 +18,20 @@ begin
     arr[i] := Random(101);
 end;
 
+procedure SortujLiczby(var arr: TIntArray);
+var
+  i, j, temp: Integer;
+begin
+  for i := 1 to ARRAY_SIZE - 1 do
+    for j := 1 to ARRAY_SIZE - i do
+      if arr[j] > arr[j + 1] then
+      begin
+        temp     := arr[j];
+        arr[j]   := arr[j + 1];
+        arr[j+1] := temp;
+      end;
+end;
+
 procedure WypiszLiczby(var arr: TIntArray);
 var
   i: Integer;
@@ -33,5 +47,9 @@ var
 begin
   GenerujLiczby(liczby);
   WriteLn('Wygenerowane liczby:');
+  WypiszLiczby(liczby);
+
+  SortujLiczby(liczby);
+  WriteLn('Posortowane liczby:');
   WypiszLiczby(liczby);
 end.
