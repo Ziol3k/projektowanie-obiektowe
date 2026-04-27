df0d27868cf36b7eeffcc513fe2381a9bc673b0b
Ziol3k <jan_ziolo@wp.pl>
2026-04-27 19:08:08 +0200

    zadanie_4 4.0 & 4.5 completed

--- zadanie_4/main.go
+++ zadanie_4/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 
 	"github.com/glebarez/sqlite"
@@ -16,6 +16,12 @@ type Weather struct {
 	Description	string	`json:"description"`
 }
 
+type ExternalWeatherResponse struct {
+	Current struct {
+		Temperature float64 `json:"temperature_2m"`
+	} `json:"current"`
+}
+
 var db *gorm.DB
 
 func initDB() {
@@ -28,20 +34,60 @@ func initDB() {
 
 	db.AutoMigrate(&Weather{})
 
-	initialData := []Weather{
-		{Location: "Warsaw", Temperature: 20.2, Description: "Cloudy"},
-		{Location: "Krakow", Temperature: 28.5, Description: "Sunny"},
-		{Location: "Wroclaw", Temperature: 19.0, Description: "Rainy"},
+	// initialData := []Weather{
+	// 	{Location: "Warsaw", Temperature: 20.2, Description: "Cloudy"},
+	// 	{Location: "Krakow", Temperature: 28.5, Description: "Sunny"},
+	// 	{Location: "Wroclaw", Temperature: 19.0, Description: "Rainy"},
+	// }
+
+	// for _, w := range initialData {
+	// 	var count int64
+	// 	db.Model(&Weather{}).Where("Location = ?", w.Location).Count(&count)
+	// 	if count == 0 {
+	// 		db.Create(&w)
+	// 		fmt.Printf("Added initial data for: %s\n", w.Location)
+	// 	}
+	// }
+}
+
+func fetchExternalWeather(city string) (float64, error) {
+	url := "https://api.open-meteo.com/v1/forecast?latitude=52.2297&longitude=21.0122&current=temperature_2m"
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return 0, err
+	}
+	defer resp.Body.Close()
+
+	var result ExternalWeatherResponse
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		return 0, err
 	}
 
-	for _, w := range initialData {
-		var count int64
-		db.Model(&Weather{}).Where("Location = ?", w.Location).Count(&count)
-		if count == 0 {
-			db.Create(&w)
-			fmt.Printf("Added initial data for: %s\n", w.Location)
-		}
+	return result.Current.Temperature, nil
+}
+
+func GetWeatherProxy(c echo.Context) error {
+	city := "Warsaw"
+
+	temp, err := fetchExternalWeather(city)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "Error fetching external weather")
 	}
+
+	newEntry := Weather{
+		Location:    city,
+		Temperature: temp,
+		Description: "Fetched from External Proxy",
+	}
+	db.Create(&newEntry)
+
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"status":  "Data fetched via Proxy and saved to DB",
+		"city":    city,
+		"temp":    temp,
+		"source":  "Open-Meteo API",
+	})
 }
 
 func GetWeather(c echo.Context) error {
@@ -55,7 +101,7 @@ func main() {
 
 	e:= echo.New()
 
-	e.GET("/weather", GetWeather)
-	e.POST("/weather", GetWeather)
+	e.GET("/weather/proxy", GetWeatherProxy)
+	e.POST("/weather/proxy", GetWeatherProxy)
 	e.Logger.Fatal(e.Start(":8081"))
 }
