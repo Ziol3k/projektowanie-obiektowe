55addcdfaba5f201c723f8c4c9c34e0ee42faabf
Ziol3k <jan_ziolo@wp.pl>
2026-04-27 18:57:01 +0200

    zadanie_4 3.5 completed

--- zadanie_4/go.mod
+++ zadanie_4/go.mod
@@ -3,6 +3,10 @@ module weather-proxy
 go 1.24.2
 
 require (
+	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/glebarez/go-sqlite v1.21.2 // indirect
+	github.com/glebarez/sqlite v1.11.0 // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/labstack/echo/v4 v4.15.1 // indirect
@@ -10,6 +14,7 @@ require (
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-sqlite3 v1.14.22 // indirect
+	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/crypto v0.46.0 // indirect
@@ -18,4 +23,8 @@ require (
 	golang.org/x/text v0.32.0 // indirect
 	gorm.io/driver/sqlite v1.6.0 // indirect
 	gorm.io/gorm v1.31.1 // indirect
+	modernc.org/libc v1.22.5 // indirect
+	modernc.org/mathutil v1.5.0 // indirect
+	modernc.org/memory v1.5.0 // indirect
+	modernc.org/sqlite v1.23.1 // indirect
 )
--- zadanie_4/main.go
+++ zadanie_4/main.go
@@ -1,18 +1,58 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/glebarez/sqlite"
 	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
 )
 
+type Weather struct {
+	gorm.Model
+	Location	string	`json:"location"`
+	Temperature	float64 `json:"temperature"`
+	Description	string	`json:"description"`
+}
+
+var db *gorm.DB
+
+func initDB() {
+	var err error
+	db, err = gorm.Open(sqlite.Open("weather.db"), &gorm.Config{})
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	db.AutoMigrate(&Weather{})
+
+	initialData := []Weather{
+		{Location: "Warsaw", Temperature: 20.2, Description: "Cloudy"},
+		{Location: "Krakow", Temperature: 28.5, Description: "Sunny"},
+		{Location: "Wroclaw", Temperature: 19.0, Description: "Rainy"},
+	}
+
+	for _, w := range initialData {
+		var count int64
+		db.Model(&Weather{}).Where("Location = ?", w.Location).Count(&count)
+		if count == 0 {
+			db.Create(&w)
+			fmt.Printf("Added initial data for: %s\n", w.Location)
+		}
+	}
+}
+
 func GetWeather(c echo.Context) error {
-	return c.JSON(http.StatusOK, map[string]string{
-		"message": "Weather controller is working!",
-	})
+	var weatherData []Weather
+	db.Find(&weatherData)
+	return c.JSON(http.StatusOK, weatherData)
 }
 
 func main() {
+	initDB()
+
 	e:= echo.New()
 
 	e.GET("/weather", GetWeather)
