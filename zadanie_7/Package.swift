8c606a728a227fc88b19e488e4f97a07ce4013bd
Ziol3k <jan_ziolo@wp.pl>
2026-05-26 21:53:17 +0200

    Prepare Heroku

--- zadanie_7/Package.swift
+++ zadanie_7/Package.swift
@@ -10,6 +10,7 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", from: "4.115.0"),
         .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.9.0"),
+        .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0"),
         .package(url: "https://github.com/vapor/leaf.git", from: "4.4.0"),
         .package(url: "https://github.com/vapor/redis.git", from: "4.0.0")
     ],
@@ -20,6 +21,7 @@ let package = Package(
                 .product(name: "Vapor", package: "vapor"),
                 .product(name: "Fluent", package: "fluent"),
                 .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
+                .product(name: "FluentPostgresDriver", package: "fluent-postgres-driver"),
                 .product(name: "Leaf", package: "leaf"),
                 .product(name: "Redis", package: "redis")
             ],
--- zadanie_7/Sources/zadanie_7/configure.swift
+++ zadanie_7/Sources/zadanie_7/configure.swift
@@ -1,15 +1,20 @@
 import Vapor
 import Fluent
 import FluentSQLiteDriver
+import FluentPostgresDriver
 import Leaf
 import Redis
 
 public func configure(_ app: Application) async throws {
-    app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
+    if let databaseURL = Environment.get("DATABASE_URL") {
+        try app.databases.use(.postgres(url: databaseURL), as: .psql)
+    } else {
+        app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
+    }
 
     app.views.use(.leaf)
 
-    let redisURL = Environment.get("REDIS_URL") ?? "redis://localhost:6379"
+    let redisURL = Environment.get("REDIS_URL") ?? Environment.get("HEROKU_REDIS_URL") ?? "redis://localhost:6379"
     app.redis.configuration = try RedisConfiguration(url: redisURL)
 
     app.migrations.add(CreateCategory())
